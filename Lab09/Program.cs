0c5293ef468ac867b59d1141adf34dbb3f0bfca0
SelloDaGod <Marcel.mk1577@gmail.com>
2023-08-02 19:05:52 -0500

    Complete Lab 9

---
+++ Lab09/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text.Json;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Lab09
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            string json = File.ReadAllText("C:\\Users\\Marce\\OneDrive\\Documents\\GitHub\\Lab09\\Lab09\\Data.json");
+            Console.WriteLine("Read file into string");
+            FeatureCollection featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json);
+            Console.WriteLine("Deserialized the json data");
+
+            Location[] locations = featureCollection.features;
+            Console.WriteLine(locations);
+            Part1WithLINQ(locations);
+        }
+        public static void Part1(Location[] items)
+        {
+            Dictionary<string, int> locationAppearances = new Dictionary<string, int>();
+            for (int i = 0; i < items.Length; i++)
+            {
+                Location currentLocation = items[i];
+                string neighborhood = currentLocation.properties.neighborhood;
+                bool neighborhoodAlreadyInDictionary = locationAppearances.ContainsKey(neighborhood);
+                if (neighborhoodAlreadyInDictionary == false)
+                {
+                    locationAppearances.Add(neighborhood, 1);
+                }
+                else
+                {
+                    int currentValue = locationAppearances.GetValueOrDefault(neighborhood);
+                    int newValue = currentValue + 1;
+                    locationAppearances[neighborhood] = newValue;
+
+                }
+            }
+
+            foreach (var location in locationAppearances)
+            {
+                Console.WriteLine($"{location.Key}: {location.Value}");
+            }
+
+        }
+
+        public static void Part1WithLINQ(Location[] items)
+        {
+            var neighborHoodQuery = from item in items
+                                    group item by item.properties.neighborhood into grouped
+                                    select new { Key = grouped.Key, Value = grouped.Count() };
+
+            foreach (var location in neighborHoodQuery)
+            {
+                Console.WriteLine($"{location.Key}: {location.Value}");
+            }
+        }
+
+    }
+}
